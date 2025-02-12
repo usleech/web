bf85465fc1c5867a43ef119695a572fae290f395
usleech <71797515+usleech@users.noreply.github.com>
2025-02-12 15:32:07 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 import os
 from flask import Flask
+from requests import get
+
 app = Flask(__name__)
+base_url = "https://124.49.55.153/"
+base_hea = {
+        'Host': 'torrentqq354.com',
+        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36',
+        }
 
 @app.route("/")
 def main():
-    return "Welcome!"
+    req = get(base_url, headers=base_hea, verify=False)
+    return req
 
 @app.route('/how are you')
 def hello():
